b23e779e78f0ef8aa80cb48942a05f7074b4045b
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-11 02:24:35 +0800

    rps v1 No design

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # odin-foundation-rockpaperscissor
 My own submission of the Project: Rock Paper Scissor by TheOdinProject
+
+I finished this on 8/11/2024
+
+Credits:
+I got inspirations for layout at the number 1 liked Submission. 
+
+Here's his github link: 
+https://github.com/michalosman/rock-paper-scissors
+
--- script.js
+++ script.js
@@ -9,16 +9,74 @@
     SHOW WINNER
 */
 
-const choices = ["rock", "paper", "scissors", "scissor"];
-let round = 1;
+const choices = ["Rock", "Paper", "Scissors", "scissor"];
+const weapons = ["✊","✋","✌️"];
+let round = 0;
+let playerLives = ["❤️", "❤️", "❤️", "❤️", "❤️"];
+let computerLives = ["❤️", "❤️", "❤️", "❤️", "❤️"];
 
+let playerWeapon = document.querySelectorAll(".button");
+let playerWeaponInfo = document.querySelector(".player-weapon");
+let computerWeaponInfo = document.querySelector(".computer-weapon");
+let winAnnounce = document.querySelector(".winner-text");
+let gameRound = document.querySelector("#round-number");
 
-let playerChose = document.querySelectorAll(".button");
+let playerLife = document.querySelector(".player-life");
+let computerLife = document.querySelector(".computer-life");
+let endText = document.querySelector(".endText");
+
 
 function computerChoice(){
     let rng = Math.floor(Math.random() * 3);
     return rng;
 }
+
+playerWeapon.forEach(weapon => {
+    weapon.addEventListener("click", e => {
+        // 0 = rock
+        // 1 = paper
+        // 2 = scissor
+        if(playerLives.length !== 0 && computerLives.length !== 0){
+            let humanChoice = parseInt(e.target.id);
+            let computer = computerChoice();
+            
+            playerWeaponInfo.textContent = weapons[humanChoice];
+            computerWeaponInfo.textContent = weapons[computer];
+
+            let outcome = playRound(humanChoice,computer);
+            
+            updateLife(outcome);
+
+            ++round;
+            gameRound.textContent = round;
+
+            
+            if(playerLives.length === 0){
+                //player lose
+                endText.textContent = "You Lose!";
+                endText.style.color = "red";
+            }else if(computerLives.length === 0){
+                endText.textContent = "You Win!";
+                endText.style.color = "green";
+            }
+        }
+    });
+});
+
+function updateLife(outcome){
+    if(outcome === 0){
+        return;
+    }else if(outcome === 1){
+        computerLives.pop();
+        computerLife.textContent = "(" + computerLives.length + ") " + computerLives.join(" ");
+    }else{
+        // outcome === 2
+        playerLives.pop();
+        playerLife.textContent = "(" + playerLives.length + ") " + playerLives.join(" ");
+        
+    }
+}
+
 function playRound(humanChoice, computerChoice){
     // playround function will return a number based on the perspetive of the human
     // 0 = draw 
@@ -26,27 +84,33 @@ function playRound(humanChoice, computerChoice){
     // 2 = lose
     if(humanChoice === computerChoice){
         //draw
-        console.log(`Game is a draw both players chose: ${choices[humanChoice]}`);
+        winAnnounce.textContent = (`Game is a DRAW both players chose: ${choices[humanChoice]}`);
+        winAnnounce.style.color = "grey";
+
         return 0;
     }else{
         if(humanChoice - computerChoice > 0){
             if(!(Math.abs(humanChoice - computerChoice) === 2)){
                 //win
-                console.log(`Human Player Wins! ${choices[humanChoice]} beats ${choices[computerChoice]}`);
+                winAnnounce.textContent = (`Human Player WINS! ${choices[humanChoice]} beats ${choices[computerChoice]}`);
+                winAnnounce.style.color = "green";
                 return 1;
             }else{
                 //lose
-                console.log(`Human Player Lose! ${choices[humanChoice]} lose against  ${choices[computerChoice]}`);
+                winAnnounce.textContent = (`Human Player LOSE! ${choices[humanChoice]} lose against  ${choices[computerChoice]}`);
+                winAnnounce.style.color = "red";
                 return 2;
             }
         }else{
             if(Math.abs(humanChoice - computerChoice) === 2){
                 //win
-                console.log(`Human Player Wins! ${choices[humanChoice]} beats ${choices[computerChoice]}`);
+                winAnnounce.textContent = (`Human Player WINS! ${choices[humanChoice]} beats ${choices[computerChoice]}`);
+                winAnnounce.style.color = "green";
                 return 1;
             }else{
                 //lose
-                console.log(`Human Player Lose! ${choices[humanChoice]} lose against  ${choices[computerChoice]}`);
+                winAnnounce.textContent = (`Human Player LOSE! ${choices[humanChoice]} lose against  ${choices[computerChoice]}`);
+                winAnnounce.style.color = "red";
                 return 2;
             }
         }
