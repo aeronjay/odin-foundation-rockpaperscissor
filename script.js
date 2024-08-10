1554be579e5e1dc512d7d106cc2dea0b1f4ed83a
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-11 01:05:54 +0800

    add html & css layout

--- script.js
+++ script.js
@@ -10,61 +10,20 @@
 */
 
 const choices = ["rock", "paper", "scissors", "scissor"];
+let round = 1;
 
-function playGame(){
-    let humanScore = 0, computerScore = 0;
 
-    let round = 1;
-    do{
-        console.log(`Round ${round}:`)
-        
-        let computer = computerChoice();
-        let human = humanChoice(round);
-        let outcome = playRound(human,computer);
-        if(outcome === 1){
-            ++humanScore;
-        }else if(outcome === 2){
-            ++computerScore;
-        }
-        console.log(`HUMAN: ${humanScore}\nCOMPUTER: ${computerScore}`)
-        ++round;
-        console.log("\n\n")
-    }while(round <= 5);
-    console.log("FINAL SCORE");
-    console.log(`HUMAN: ${humanScore}\nCOMPUTER: ${computerScore}`);
-}
+let playerChose = document.querySelectorAll(".button");
 
-let computerChoice = () => {
+function computerChoice(){
     let rng = Math.floor(Math.random() * 3);
     return rng;
-};
-
-let humanChoice = (round) => {
-    let humanPrompt = null;
-    let humanChoice = null;
-    do{
-        humanPrompt = prompt(`ROCK PAPER SCISSOR ROUND: ${round} \n\n What Is Your Choice Human?`).toLowerCase();
-    }while(humanPrompt === "" || !(choices.includes(humanPrompt)));
-    
-    switch(humanPrompt){
-        case "rock":
-            humanChoice = 0;
-            break;
-        case "paper":
-            humanChoice = 1;
-            break;
-        case "scissor":
-        case "scissors":
-            humanChoice = 2;
-            break;
-    }
-
-    return humanChoice;
-};
-
+}
 function playRound(humanChoice, computerChoice){
     // playround function will return a number based on the perspetive of the human
-    // 0 = draw 1 = win 2 = lose
+    // 0 = draw 
+    // 1 = win 
+    // 2 = lose
     if(humanChoice === computerChoice){
         //draw
         console.log(`Game is a draw both players chose: ${choices[humanChoice]}`);
@@ -94,5 +53,3 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-
-playGame();
